07e243cedfaad3abbb2214ee84ac522932e0921a
harshit nayak <harshitnayak45@gmail.com>
2019-12-12 11:22:58 +0530

    update

--- API/controller/config.js
+++ API/controller/config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    USER: 'admin@setitandleaveit.com', 
-    PASS: 'jtM$2%Gi#52E',
-    USERFROM: 'info@SETITANDLEAVEIT.com',
-    SENDGRID_API_KEY:'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',
-    domain:'http://ec2-18-221-255-18.us-east-2.compute.amazonaws.com'
-    //domain:'http://localhost:3000'
+    USER: 'xxxx@xxxx.com', 
+    PASS: 'xxxxxxxxxxx',
+    USERFROM: 'xxxxx@xxxxx.com',
+    SENDGRID_API_KEY:'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    domain:'xxxxxxxxxxxxxx.com'
 }
